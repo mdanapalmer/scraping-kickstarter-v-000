da176f02e762a46f0bed84050e06b6ccab453141
Dana Palmer <roiai4@hotmail.com>
2018-11-04 03:30:49 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,8 +9,9 @@ end
 projects = {}
 
 kickstarter.css("li.project.grid_4").each do |project|
-  projects[project] = {}
-end
+  title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {}
+    end
 
 projects
 end
