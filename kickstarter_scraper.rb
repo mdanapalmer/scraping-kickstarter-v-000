93f7ab2ce1299c23d64b110d888a73102479f08e
Dana Palmer <roiai4@hotmail.com>
2018-11-04 02:58:17 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'pry'
 
 class Scraper
 
@@ -6,7 +7,9 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
 
 
 end
+create_project_hash
